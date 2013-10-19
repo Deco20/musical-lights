6f59d9c267acf30ea220b91ca81592bcb8fcfcbb
s3331816 <s3331816@student.rmit.edu.au>
2013-10-19 12:40:43 +0000

    final play.pl update

--- play.pl
+++ play.pl
@@ -185,3 +185,82 @@ if(my $aplaymidiID = `pidof $p3` eq ($p3_id))
 `sudo renice 0 -p $p2_id`;
 ReadMode 0;
 exit;        
+   
+
+#For process 2 "timidity"
+my $sum_p2_mem = my $avg_p2_mem = 0;
+$sum_p2_mem += ($_ - $p2_mem_deduction) for @p2_mem;
+my $len_p2_mem = @p2_mem;
+$avg_p2_mem = $sum_p2_mem / $len_p2_mem;
+
+#For process 3 "aplaymidi"
+my $sum_p3_mem = my $avg_p3_mem = 0;
+$sum_p3_mem += $_ for @p3_mem;
+my $len_p3_mem = @p3_mem;
+$avg_p3_mem = $sum_p3_mem / $len_p3_mem; 
+
+#For process 4 "perl play.pl"
+my $sum_p4_mem = my $avg_p4_mem = 0;
+$sum_p4_mem += $_ for @p4_mem;
+my $len_p4_mem = @p4_mem;
+$avg_p4_mem = $sum_p4_mem / $len_p4_mem;  
+
+
+#Total CPU time for daemon processes
+$p1_time = (&convertTime($p1_time) - $p1_time_deduction);
+$p2_time = (&convertTime($p2_time) - $p2_time_deduction);
+
+
+#display test results to user
+print("Final Percentage Averages\n");
+print("-------------------------\n\n");
+printf("%-13s %-5s %-8s %-8s %-12s\n", 'Process', 'PID', 'CPU %', 'MEM %', 'CPU Time (s)');
+printf("------------- ----- -------- -------- ------------\n");
+printf("%-13s %5d %8.2f %8.2f %12s\n", $p1, $p1_id, $avg_p1_cpu, $avg_p1_mem, $p1_time);
+printf("%-13s %5d %8.2f %8.2f %12s\n", $p2, $p2_id, $avg_p2_cpu, $avg_p2_mem, $p2_time);
+printf("%-13s %5d %8.2f %8.2f %12s\n", $p3, $p3_id, $avg_p3_cpu, $avg_p3_mem, &convertTime($p3_time));
+printf("%-13s %5d %8.2f %8.2f %12s\n\n", $p4, $p4_id, $avg_p4_cpu, $avg_p4_mem, &convertTime($p4_time));
+
+#Write CPU usage arrays to txt files
+&toFile($p1, @p1_cpu);
+&toFile($p2, @p2_cpu);
+&toFile($p3, @p3_cpu);
+&toFile($p4, @p4_cpu);
+
+`sudo renice 0 -p $p2_id`;
+ReadMode 0;
+exit;
+
+#Subroutine to Write an array out to an appropriately named file
+sub toFile
+{
+   my $name = shift."_cpu.txt";
+   my @data = @_;
+   my $index = 1;
+
+   open FH, ">$name" or
+   die "Cannot open '$name'";
+   
+   foreach(@data)
+   {
+      
+      printf FH ($index * $interval)."\t$_";
+      $index++;
+   }
+
+   close FH;
+}
+
+# subroutine to convert CPU time to seconds.
+sub convertTime
+{
+   my $timeIn = $_[0];
+   my @time = split /:/ ,$timeIn;
+   my $timeSec = 0;
+   $timeSec += ($time[0] * 3600);
+   $timeSec += ($time[1] * 60);
+   $timeSec += $time[2];
+
+   return $timeSec;
+}
+
